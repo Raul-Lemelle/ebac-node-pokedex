1061c705a0775141a58623d548ad8c2d706a76bf
${GITHUB_USER} <${GITHUB_MAIL}>
2024-07-02 18:42:23 -0300

    add files de controler

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const { connect } = require('./models');
+
+const pokemonRouter = require('./routes/pokemons');
+
+const app = express();
+
+// declarando rotas
+app.use('/pokemons', pokemonsRouter);
+
+app.listen(3000, () => {
+    connect();
+
+    console.log(`Servidor ouvindo na porta ${porta}`);
+});
